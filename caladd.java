ba3f62b1484db004fec6f597838873ac86144dc0
Huskarl216 <mahithbhima@gmail.com>
2019-01-17 15:33:10 +0530

    adding sum

---
+++ caladd.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class caladd{
+	public static void main(String args[]){
+		float a,b,res;
+		char choice ,ch;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		do{
+			System.out.println("1.Add two numbers\n");
+			System.out.println("2. Exit\n");
+			System.out.println("Enter your choice: ");
+			choice=scan.next().charAt(0);
+			switch(choice){
+				case '1':	System.out.print("Enter two numbers: ");
+							a=scan.nextFloat();
+							b=scan.nextFloat();
+							res=a+b;
+							System.out.println("Result = "+ res);
+							break;
+				case '2':	System.exit(0);
+							break;
+				default:	System.out.println("INVALID CHOICE!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+							break;
+				
+			}
+			System.out.print("\n-------------------------------------------------\n");
+		}while(choice!=2);
+	}
+}
