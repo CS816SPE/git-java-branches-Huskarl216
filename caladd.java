7f5f6d765f549c2978507ee63034d15293bb1f4a
Huskarl216 <mahithbhima@gmail.com>
2019-01-17 16:18:29 +0530

    Subtraction added

--- caladd.java
+++ caladd.java
@@ -9,6 +9,7 @@ public class caladd{
 		do{
 			System.out.println("1.Add two numbers\n");
 			System.out.println("2. Exit\n");
+			System.out.println("3.Subtract two numbers\n");
 			System.out.println("Enter your choice: ");
 			choice=scan.next().charAt(0);
 			switch(choice){
@@ -19,6 +20,12 @@ public class caladd{
 							System.out.println("Result = "+ res);
 							break;
 				case '2':	System.exit(0);
+							break;
+				case '3':	System.out.print("Enter two numbers: ");
+							a=scan.nextFloat();
+							b=scan.nextFloat();
+							res=a-b;
+							System.out.println("Result = "+ res);
 							break;
 				default:	System.out.println("INVALID CHOICE!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 							break;
